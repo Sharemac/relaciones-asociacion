baa20c27e80511118786d7a9864a82760118c3f1
Estudiante <Estudiante@10.203.2.198>
2017-03-02 15:52:18 -0500

    asociacion hacer para composicion

---
+++ src/relacionn/Main.java
@@ -0,0 +1,25 @@
+
+package relacionn;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       Estudiante estudiante = new Estudiante();
+       Materia m1 = new Materia(1);
+       Materia m2 = new Materia(2);
+       Materia m3 = new Materia(3);
+       Materia m4 = new Materia(4);
+       Materia m5 = new Materia(5);
+       Materia m6 = new Materia(6);
+       
+       estudiante.addMateria(m1);
+       estudiante.addMateria(m2);
+       estudiante.addMateria(m3);
+       estudiante.addMateria(m4);
+       estudiante.addMateria(m5);
+       estudiante.addMateria(m6);
+       estudiante.imprimirMaterias();
+    }
+    
+}
